3c7b6145402e8cb1a6c4a7736712fbfbadc7cbce
alynn520 <alynn202309@gmail.com>
2026-07-07 04:26:29 +0900

    modify end point

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api/v1');
+
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
 
